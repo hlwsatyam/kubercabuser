0a54da03d242aedb6795a1e1f10127fe87e7f7e7
hlwsatyam <satyampandit021@gmail.com>
2025-11-08 18:12:46 +0530

    sdffds

--- config/firebaseApps.js
+++ config/firebaseApps.js
@@ -1,19 +1,19 @@
 const admin = require('firebase-admin');
 
 const app1ServiceAccount = require('./serviceAccountKey.json');
-const app2ServiceAccount = require('./kubercabdriver-ab5bb-firebase-adminsdk-fbsvc-f10a858fb9.json');
+// const app2ServiceAccount = require('./kubercabdriver-ab5bb-firebase-adminsdk-fbsvc-f10a858fb9.json');
 
 // 🔹 Initialize both apps
 const firebaseKuberCab = admin.initializeApp({
   credential: admin.credential.cert(app1ServiceAccount),
 }, 'kubercab');
 
-const firebaseKuberCabDriver = admin.initializeApp({
-  credential: admin.credential.cert(app2ServiceAccount),
-}, 'kubercabDriver');
+// const firebaseKuberCabDriver = admin.initializeApp({
+//   credential: admin.credential.cert(app2ServiceAccount),
+// }, 'kubercabDriver');
 
 // 🔹 Export both for use in routes/controllers
 module.exports = {
-  firebaseKuberCab,
-  firebaseKuberCabDriver,
+  firebaseKuberCab
+  // firebaseKuberCabDriver,
 };
